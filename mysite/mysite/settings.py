c0c44a946511e7c19596cd9f0d8174ee5fb06169
Ramon Domingos <ramongemio@gmail.com>
2018-10-05 10:13:29 -0300

    tutorial2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,15 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE_CLASSES = [
-'django.contrib.auth.middleware.AuthenticationMiddleware',
-'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+MIDDLEWARE = [
+
+     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
